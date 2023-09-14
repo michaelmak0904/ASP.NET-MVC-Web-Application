0cd5a7dd72987c3d0532a2721036e1618bfed8fa
Michael Mak <michaelmak0904@gmail.com>
2023-09-14 14:59:15 +0800

    Create and Read

--- hello/Controllers/EmployeeController.cs
+++ hello/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using hello.Data;
+using hello.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,6 +22,18 @@ namespace hello.Controllers
         {
             var employees = _context.Employees.ToList();
             return View(employees);
+        }
+        // GET: /<controller>/
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee) {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return View();
         }
     }
 }
